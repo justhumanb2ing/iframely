a7aba3913e549e17291ef03ad61242ff54ca1a41
coggiee <zentechie7@gmail.com>
2026-04-29 13:55:34 +0900

    Reduce cluster workers on Render

--- cluster.js
+++ cluster.js
@@ -3,12 +3,15 @@ import * as sysUtils from './utils.js';
 
 process.title = 'iframely-cluster';
 
+const webConcurrency = Number(process.env.WEB_CONCURRENCY);
+
 GracefulCluster.start({
     log: sysUtils.log,
     shutdownTimeout: CONFIG.SHUTDOWN_TIMEOUT,
     disableGraceful: CONFIG.DEBUG,
     restartOnTimeout: CONFIG.CLUSTER_WORKER_RESTART_ON_PERIOD,
     restartOnMemory: CONFIG.CLUSTER_WORKER_RESTART_ON_MEMORY_USED,
+    workersCount: Number.isFinite(webConcurrency) && webConcurrency > 0 ? webConcurrency : undefined,
     serverFunction: function() {
         import('./server.js');
     }
